d3e410c42c97503195888d9dbb667c61f06b8687
Rachel Miles <rachmiles@gmail.com>
2022-05-11 18:43:12 +0100

    Initial implementation of conversion from roman to arabic

--- app/repositories/RomansRepository.scala
+++ app/repositories/RomansRepository.scala
@@ -6,13 +6,12 @@ import javax.inject.Singleton
 @Singleton
 class RomansRepository {
   private val romans: Map[Int, String] = Map( 1 -> "I", 5 -> "V")
-
+  private val sortedArabics: Seq[Int] = romans.keys.toSeq.sortWith( _ > _ )
 
   def convertToRoman( arabic: Int ): Conversion = {
-    val arabics: Seq[Int] = romans.keys.toSeq.sortWith( _ > _ )
     var workingNum: Int = arabic
     var roman = ""
-    for ( arabicNum <- arabics ) {
+    for ( arabicNum <- sortedArabics ) {
       while (workingNum >= arabicNum) {
         workingNum -= arabicNum
         roman += romans(arabicNum)
@@ -22,6 +21,15 @@ class RomansRepository {
   }
 
   def convertToArabic( roman: String ): Conversion = {
-    Conversion(1, roman)
+    var workingRoman: String = roman
+    var arabic = 0
+    val romanChars: Seq[String] = sortedArabics.map(arabic => romans(arabic))
+    for (romanChar <- romanChars) {
+      while (workingRoman.slice(0, romanChar.length) == romanChar) {
+        arabic += romans.filter(_._2 == romanChar).keys.head
+        workingRoman = workingRoman.substring( romanChar.length )
+      }
+    }
+    Conversion(arabic, roman)
   }
 }
