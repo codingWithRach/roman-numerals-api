5b51b65bcb78ce124fc28b697921fed4867ccdd9
Rachel Miles <rachmiles@gmail.com>
2022-05-12 13:15:12 +0100

    Expand definition to cover all Roman numerals and increase tests

--- app/repositories/RomansRepository.scala
+++ app/repositories/RomansRepository.scala
@@ -5,7 +5,20 @@ import javax.inject.Singleton
 
 @Singleton
 class RomansRepository {
-  private val romans: Map[Int, String] = Map( 1 -> "I", 5 -> "V")
+  private val romans: Map[Int, String] = Map(
+    1 -> "I",
+    4 -> "IV",
+    5 -> "V",
+    9 -> "IX",
+    10 -> "X",
+    40 -> "XL",
+    50 -> "L",
+    90 -> "XC",
+    100 -> "C",
+    400 -> "CD",
+    500 -> "D",
+    900 -> "CM",
+    1000 -> "M")
   private val sortedArabics: Seq[Int] = romans.keys.toSeq.sortWith( _ > _ )
 
   def convertToRoman( arabic: Int ): Conversion = {
--- test/repositories/RomansRepositorySpec.scala
+++ test/repositories/RomansRepositorySpec.scala
@@ -9,11 +9,27 @@ class RomansRepositorySpec extends AnyFlatSpec with Matchers {
   val dataRepository = new RomansRepository
 
   "convertToRoman" should "convert the specified arabic number to the correct Roman numerals" in {
-    dataRepository.convertToRoman(6) should be (Conversion(6, "VI"))
+    dataRepository.convertToRoman(1) should be (Conversion(1, "I"))
+    dataRepository.convertToRoman(7) should be (Conversion(7, "VII"))
+    dataRepository.convertToRoman(14) should be (Conversion(14, "XIV"))
+    dataRepository.convertToRoman(1972) should be (Conversion(1972, "MCMLXXII"))
   }
 
   "convertToArabic" should "convert the specified Roman numerals to the correct arabic number" in {
-    dataRepository.convertToArabic("VI") should be (Conversion(6, "VI"))
+    dataRepository.convertToArabic("IV") should be (Conversion(4, "IV"))
+    dataRepository.convertToArabic("VIII") should be (Conversion(8, "VIII"))
+    dataRepository.convertToArabic("XXVII") should be (Conversion(27, "XXVII"))
+    dataRepository.convertToArabic("MCMLXXII") should be (Conversion(1972, "MCMLXXII"))
+  }
+
+  "convertToRoman followed by convertToArabic" should "result in the original arabic number" in {
+    val conversion: Conversion = dataRepository.convertToRoman(6)
+    dataRepository.convertToArabic(conversion.roman) should be (conversion)
+  }
+
+  "convertToArabic followed by convertToRoman" should "result in the original Roman numeral" in {
+    val conversion: Conversion = dataRepository.convertToArabic("XII")
+    dataRepository.convertToRoman(conversion.arabic) should be (conversion)
   }
 
 }
