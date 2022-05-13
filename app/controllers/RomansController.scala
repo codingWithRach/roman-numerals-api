550f7beff09a8c0c9b217fc076c0c17fc0b33885
Rachel Miles <rachmiles@gmail.com>
2022-05-13 16:32:59 +0100

    Refactor Controller

--- app/controllers/RomansController.scala
+++ app/controllers/RomansController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import models.{Arabic, Conversion, Roman}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Result}
 import play.api.libs.json._
 
 import javax.inject.{Inject, Singleton}
@@ -9,77 +9,45 @@ import repositories.RomansRepository
 
 @Singleton
 class RomansController  @Inject()(val controllerComponents: ControllerComponents, dataRepository: RomansRepository) extends BaseController {
+
   private val emptyConversion = Conversion(0, "")
 
   def convertToRoman(arabic: Int): Action[AnyContent] = Action {
-    val conversion: Conversion = dataRepository.convertToRoman(arabic)
-    val response = checkConversion(conversion, "Number cannot be converted into Roman numerals")
-    if (conversion == emptyConversion)
-      BadRequest(response)
-    else {
-      Ok(response)
-    }
+    getResponse(dataRepository.convertToRoman(arabic), "Number cannot be converted into Roman numerals")
   }
 
   def convertBodyToRoman(): Action[AnyContent] = Action {
     implicit request => {
-      val requestBody = request.body
-      val arabicAsJson = requestBody.asJson
-      val arabic: Option[Arabic] =
-        arabicAsJson.flatMap(
-          Json.fromJson[Arabic](_).asOpt
-        )
+      val arabic: Option[Arabic] = request.body.asJson.flatMap(Json.fromJson[Arabic](_).asOpt)
       if (arabic.isEmpty) {
         BadRequest(Json.toJson("Request body not in required format"))
       } else {
-        val conversion: Conversion = dataRepository.convertToRoman(arabic.get.arabic)
-        val response = checkConversion(conversion, "Number cannot be converted into Roman numerals")
-        if (conversion == emptyConversion)
-          BadRequest(response)
-        else {
-          Ok(response)
-        }
+        getResponse(dataRepository.convertToRoman(arabic.get.arabic), "Number cannot be converted into Roman numerals")
       }
     }
   }
 
   def convertToArabic(roman: String): Action[AnyContent] = Action {
-    val conversion: Conversion = dataRepository.convertToArabic(roman)
-    val response = checkConversion(conversion, "Not a valid Roman numeral")
-    if (conversion == emptyConversion)
-      BadRequest(response)
-    else {
-      Ok(response)
-    }
+    getResponse(dataRepository.convertToArabic(roman), "Not a valid Roman numeral")
   }
 
   def convertBodyToArabic(): Action[AnyContent] = Action {
     implicit request => {
-      val requestBody = request.body
-      val romanAsJson = requestBody.asJson
-      val roman: Option[Roman] =
-        romanAsJson.flatMap(
-          Json.fromJson[Roman](_).asOpt
-        )
+      val roman: Option[Roman] = request.body.asJson.flatMap(Json.fromJson[Roman](_).asOpt)
       if (roman.isEmpty) {
         BadRequest(Json.toJson("Request body not in required format"))
       } else {
-        val conversion: Conversion = dataRepository.convertToArabic(roman.get.roman)
-        val response = checkConversion(conversion, "Not a valid Roman numeral")
-        if (conversion == emptyConversion)
-          BadRequest(response)
-        else {
-          Ok(response)
-        }
+        getResponse(dataRepository.convertToArabic(roman.get.roman), "Not a valid Roman numeral")
       }
     }
   }
 
-  def checkConversion(conversion: Conversion, message: String): JsValue = {
+  def getResponse(conversion: Conversion, message: String): Result = {
     if (conversion == emptyConversion) {
-      Json.toJson(message)
-    } else {
-      Json.toJson(conversion)
+        BadRequest(Json.toJson(message))
+      } else {
+        Ok(Json.toJson(conversion))
+      }
     }
-  }
+
 }
